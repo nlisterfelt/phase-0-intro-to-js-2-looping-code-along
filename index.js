0350269cfdb987c5a1969a6f688e8cef984d1dc9
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-06-02 16:32:56 -0500

    thank you cards and counting down

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return array
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown(int){
+    for(let i = 0; i <= int; i++){
+        console.log(int-i)
+    }
+}
+countDown(10)
